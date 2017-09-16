401da36bd89f28d9e8faf55d852ff6d59b7dbb2a
Andriy <31997824+bublyk69@users.noreply.github.com>
2017-09-16 23:31:35 +0300

    Update main_window.java

--- main_window.java
+++ main_window.java
@@ -1,8 +1,11 @@
 import java.io.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.FileNotFoundException;
@@ -28,22 +31,54 @@ public  main_window() throws FileNotFoundException, IOException, ParseException{
 	JTabbedPane tabs = new JTabbedPane();
 	JFrame frame = new JFrame("crm");
 	frame.setSize(750, 600);
-	JPanel panel = new JPanel(); 
+	JPanel mainPanel = new JPanel(); 
 	JPanel customers = new JPanel();
 	frame.add(tabs);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setVisible(true);
-	frame.add(panel, BorderLayout.CENTER);
-	panel.setLayout(null);
-	tabs.add("1", panel);
+	frame.add(mainPanel, BorderLayout.CENTER);
+	mainPanel.setLayout(null);
+	tabs.add("1", mainPanel);
 	tabs.addTab("2", customers);
 	frame.setLocationRelativeTo(null);
     frame.setVisible(true);
+    
+    DefaultTableModel model = new DefaultTableModel(); 
+	// Create a couple of columns 
+	model.addColumn("name"); 
+	model.addColumn("surname"); 
+	model.addColumn("fathername");
+	model.addColumn("phone");
+	model.addColumn("e-mail");
+	model.addColumn("date of birth");
+	model.addColumn("date of visit");
+	// Append a row 
+	JTable customerList = new JTable(model); 
+    JScrollPane sp = new JScrollPane(customerList); 
+    sp.setPreferredSize(new Dimension(600,500));
+    customers.add(sp);
+    JButton delBtn = new JButton("delete");
+	customers.add(delBtn);
+	delBtn.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			int selRow = customerList.getSelectedRow();
+            if(selRow != -1) {
+                model.removeRow(selRow);
+                
+            }
+		}
+	});
+	for(int i = 0; i < list.size(); i++)
+    {
+        JSONObject obj = (JSONObject) list.get(i);
+        model.addRow(new Object[]{obj.get("name"), obj.get("surname"), obj.get("fathername"), obj.get("phone"), obj.get("e-mail"), obj.get("date1"), obj.get("date2")});
+    }
+	
 	
 	name = new JTextField("Input your name");
 	name.setToolTipText("Input your name");
 	name.setBounds(20, 20, 200, 30);
-	panel.add(name);
+	mainPanel.add(name);
 	name.setFocusTraversalKeysEnabled(false);
 	name.addFocusListener(new FocusListener() {
 		public void focusLost(FocusEvent e) {}
@@ -66,7 +101,7 @@ public  main_window() throws FileNotFoundException, IOException, ParseException{
 	surname = new JTextField("Input your surname");
 	surname.setBounds(20,60, 200,30);
 	surname.setToolTipText("Input your surname");
-	panel.add(surname);
+	mainPanel.add(surname);
 	surname.setFocusTraversalKeysEnabled(false);
 	surname.addFocusListener(new FocusListener() {
 		public void focusLost(FocusEvent e) {}
@@ -89,7 +124,7 @@ public  main_window() throws FileNotFoundException, IOException, ParseException{
 	fathername = new JTextField("Input your fathername");
 	fathername.setBounds(20,100, 200,30);
 	fathername.setToolTipText("Input your fathername");
-	panel.add(fathername);
+	mainPanel.add(fathername);
 	fathername.setFocusTraversalKeysEnabled(false);
 	fathername.addFocusListener(new FocusListener() {
 		public void focusLost(FocusEvent e) {}
@@ -112,7 +147,7 @@ public  main_window() throws FileNotFoundException, IOException, ParseException{
 	dateofbirth = new JTextField("Input your date of birth");
 	dateofbirth.setBounds(350,20, 130,30);
 	dateofbirth.setToolTipText("Input date of your birth");
-	panel.add(dateofbirth);
+	mainPanel.add(dateofbirth);
 	dateofbirth.setFocusTraversalKeysEnabled(false);
 	dateofbirth.addFocusListener(new FocusListener() {
 		public void focusLost(FocusEvent e) {}
@@ -135,7 +170,7 @@ public  main_window() throws FileNotFoundException, IOException, ParseException{
 	dateofvisit = new JTextField("Input your date of visit");
 	dateofvisit.setBounds(350,60, 130,30);
 	dateofvisit.setToolTipText("Input date of your visit");
-	panel.add(dateofvisit);
+	mainPanel.add(dateofvisit);
 	dateofvisit.setFocusTraversalKeysEnabled(false);
 	dateofvisit.addFocusListener(new FocusListener() {
 		public void focusLost(FocusEvent e) {}
@@ -158,7 +193,7 @@ public  main_window() throws FileNotFoundException, IOException, ParseException{
 	phone = new JTextField("Input your phone number");
 	phone.setBounds(20,220, 200,30);
 	phone.setToolTipText("Input your phone number");
-	panel.add(phone);
+	mainPanel.add(phone);
 	phone.setFocusTraversalKeysEnabled(false);
 	phone.addFocusListener(new FocusListener() {
 		public void focusLost(FocusEvent e) {}
@@ -181,7 +216,7 @@ public  main_window() throws FileNotFoundException, IOException, ParseException{
 	mail = new JTextField("Input your e-mail");
 	mail.setBounds(20,260, 200,30);
 	mail.setToolTipText("Input your e-mail");
-	panel.add(mail);
+	mainPanel.add(mail);
 	mail.setFocusTraversalKeysEnabled(false);
 	mail.addFocusListener(new FocusListener() {
 		public void focusLost(FocusEvent e) {}
@@ -203,7 +238,7 @@ public  main_window() throws FileNotFoundException, IOException, ParseException{
 	
 	ok = new JButton("OK");
 	ok.setBounds(0, 450, 650, 50);
-	panel.add(ok);
+	mainPanel.add(ok);
 	ok.setFocusTraversalKeysEnabled(false);
 	ok.addKeyListener(new KeyListener() {
 		public void keyTyped(KeyEvent e) {}
@@ -256,7 +291,7 @@ public  main_window() throws FileNotFoundException, IOException, ParseException{
 	close.setSize(100, 50);
 	close.setLocation(400, 250);
 	close.addActionListener(e -> System.exit(0));
-	panel.add(close);
+	mainPanel.add(close);
 	close.setFocusTraversalKeysEnabled(false);
 	close.addKeyListener(new KeyListener() {
 		public void keyTyped(KeyEvent e) {}
@@ -268,23 +303,8 @@ public  main_window() throws FileNotFoundException, IOException, ParseException{
 			}
 		}
 	});
+		
 	
-	DefaultTableModel model = new DefaultTableModel(); 
-	// Create a couple of columns 
-	model.addColumn("name"); 
-	model.addColumn("surname"); 
-	model.addColumn("fathername");
-	model.addColumn("phone");
-	model.addColumn("e-mail");
-	model.addColumn("date of birth");
-	model.addColumn("date of visit");
-	// Append a row 
-	model.addRow(new Object[]{"ander", "honch", "bogdanov", "09323456", "123456khn", "56789", "5678"});
-	model.addRow(new Object[]{"ander", "honch", "bogdanov", "09323456", "123456khn", "56789", "5678"});
-	JTable customerList = new JTable(model);    
-    JScrollPane sp = new JScrollPane(customerList); 
-    sp.setPreferredSize(new Dimension(600,500));
-    customers.add(sp);
 	frame.setPreferredSize(new Dimension(750, 601));
     frame.pack();   
 }
