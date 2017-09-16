8146ebe8b8333d9f8e00856d35e170aa911db855
vladiklem <lemvlad99@gmail.com>
2017-09-16 13:30:46 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+
+public class main {
+	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException{
+	new main_window();
+	}
+}
