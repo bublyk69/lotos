0d7fcc4d6e4cd012b6756201d2acdef921565f5a
Andriy <31997824+bublyk69@users.noreply.github.com>
2017-09-19 17:51:53 +0300

    Update mail_managment.java

--- mail_managment.java
+++ mail_managment.java
@@ -5,7 +5,7 @@ import javax.mail.internet.*;
 import javax.activation.*;
 
 
-public class mail_managment {
+public class mail_manager {
 
 
 
@@ -46,4 +46,41 @@ public class mail_managment {
       }
       catch (MessagingException e) {throw new RuntimeException(e);}
    }
+
+public static void mail_remind(String to, String name ) {    
+    // Recipient's email ID needs to be mentioned.
+	  String from = "19bublyk99@gmail.com";
+	  String fromPass = "andriy69";
+    String text = "Шановний" + name + ", з часу Вашого останнього візиту пройшло півроку,ми наполегливо радимо Вам відвідати стоматолога найближчим часом";
+    
+    //Get the session object
+    Properties props = new Properties();
+    props.put("mail.smtp.host", "smtp.gmail.com");
+    props.put("mail.smtp.socketFactory.port", "465");
+    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.port", "465");
+    
+    Session session = Session.getDefaultInstance(props, 
+      new javax.mail.Authenticator() {
+   protected PasswordAuthentication getPasswordAuthentication() {
+      return new PasswordAuthentication(from, fromPass);
+      //change according to your information
+   }
+    });
+    
+    try {
+     MimeMessage message = new MimeMessage(session);
+     message.setFrom(new InternetAddress(from));
+     //change accordingly
+     message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        message.setSubject("Вітання з Днем Народження");
+        message.setText(text);
+        
+        //send message
+        Transport.send(message);
+        System.out.println("message sent successfully");
+    }
+    catch (MessagingException e) {throw new RuntimeException(e);}
+ }
 }
